e049764e09826be23955dd974fcbc09f4f7936f6
Arseniy <arsmatus@list.ru>
2021-11-11 20:48:20 +0300

    resolve problem w/several users

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from string import Template
 
 import telebot
 from telebot import types
+from telegram.ext import run_async
 
 bot = telebot.TeleBot('2037001514:AAGj5hIXM7nrsKd_2H6PKqBsGowFhWpzypE')
 
@@ -15,16 +16,22 @@ class User:
             self.d = None
 
 
-@bot.message_handler(commands=['start'])
-def start_message(msg):
+
+def get_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=False)
     about = types.KeyboardButton('/about')
-    reg = types.KeyboardButton('/reg')
     info = types.KeyboardButton('/info')
-    markup.add(about, reg, info)
-    bot.send_message(msg.chat.id, 'Добро пожаловать', reply_markup=markup)
+    reg = types.KeyboardButton('/reg')
+    markup.add(about, info, reg)
+    return markup
+
+@run_async
+@bot.message_handler(commands=['start'])
+def start_message(msg):
+    bot.send_message(msg.chat.id, 'Добро пожаловать', reply_markup=get_markup())
 
 
+@run_async
 @bot.message_handler(commands=['about'])
 def about_message(msg):
     bot.send_message(msg.chat.id, "О нас: TODO.")
@@ -33,9 +40,10 @@ def about_message(msg):
 user_dict = {}
 
 
+@run_async
 @bot.message_handler(commands=['reg'])
 def reg_message(msg):
-    question = 'Приступим к регистрации! Вас зовут ' \
+    question = 'Приступим! Вас зовут ' \
                + msg.from_user.first_name + ' ' \
                + msg.from_user.last_name + '?'
     keyboard = types.InlineKeyboardMarkup()
@@ -45,17 +53,18 @@ def reg_message(msg):
     keyboard.add(key_no)
     bot.send_message(msg.chat.id, text=question, reply_markup=keyboard)
 
-    @bot.callback_query_handler(func=lambda call: True)
+    @run_async
+    @bot.callback_query_handler(func=lambda call: call.message.chat.id == msg.chat.id)
     def callback_worker(call):
         chat_id = call.message.chat.id
         if call.data == "yes":
-            bot.edit_message_text(text=question, chat_id=call.message.chat.id, message_id=call.message.message_id,
+            bot.edit_message_text(text=question, chat_id=chat_id, message_id=call.message.message_id,
                                   reply_markup=None)
             user_dict[chat_id] = User(chat_id, msg.from_user.first_name + ' ' + msg.from_user.last_name)
             bot.send_message(chat_id, "Сколько вам лет?")
             bot.register_next_step_handler(msg, get_age)
         else:
-            bot.edit_message_text(text=question, chat_id=call.message.chat.id, message_id=call.message.message_id,
+            bot.edit_message_text(text=question, chat_id=chat_id, message_id=call.message.message_id,
                                   reply_markup=None)
             bot.send_message(chat_id, "А как тогда?")
             bot.register_next_step_handler(msg, get_name)
@@ -64,45 +73,45 @@ def reg_message(msg):
 def get_name(msg):
     chat_id = msg.chat.id
     user_dict[chat_id] = User(chat_id, msg.text)
-    bot.send_message(msg.chat.id, "Сколько вам лет?")
+    bot.send_message(chat_id, "Сколько вам лет?")
     bot.register_next_step_handler(msg, get_age)
 
 
 def get_age(msg):
     try:
-        user = user_dict[msg.chat.id]
-        user.age = int(msg.text)
-        if user.age < 0 or user.age > 120:
-            msg = bot.send_message(user.id, 'Некорректный возраст')
-        else:
-            phone_keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-            req_button = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
-            phone_keyboard.add(req_button)
-            msg = bot.send_message(user.id, 'Ваш номер телефона', reply_markup=phone_keyboard)
-            # msg = bot.send_message(user.id, 'Ваш номер телефона:')
-            bot.register_next_step_handler(msg, get_phone)
+        if msg.text != '/stop':
+            user = user_dict[msg.chat.id]
+            user.age = int(msg.text)
+            if user.age < 0 or user.age > 120:
+                msg = bot.send_message(user.id, 'Некорректный возраст')
+                bot.register_next_step_handler(msg, get_age)
+            else:
+                phone_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+                req_button = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+                phone_keyboard.add(req_button)
+                msg = bot.send_message(user.id, 'Ваш номер телефона', reply_markup=phone_keyboard)
+                bot.register_next_step_handler(msg, get_phone)
 
     except ValueError:
-        msg = bot.send_message(user.id, 'Цифрами, пожалуйста')
+        msg = bot.send_message(user.id, 'Цифрами, пожалуйста. Для отмены регистрации нажмите /stop')
         bot.register_next_step_handler(msg, get_age)
     except KeyError:
         bot.send_message(user.id, 'Ошибка регистрации, начните заново /reg')
 
 
 def get_phone(msg):
-    try:
-        user = user_dict[msg.chat.id]
-        user.phoneNumber = msg.contact.phone_number
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=False)
-        about = types.KeyboardButton('/about')
-        info = types.KeyboardButton('/info')
-        markup.add(about, info)
-        bot.send_message(user.id, 'Регистрация прошла успешно', reply_markup=markup)
-    except (ValueError, AttributeError):
-        msg = bot.send_message(user.id, 'Неверный номер телефона')
-        bot.register_next_step_handler(msg, get_phone)
-    except KeyError:
-        bot.send_message(user.id, 'Ошибка регистрации, начните заново /reg')
+    if msg.text != '/stop':
+        try:
+            user = user_dict[msg.chat.id]
+            user.phoneNumber = msg.contact.phone_number
+            bot.send_message(user.id, 'Регистрация прошла успешно', reply_markup=get_markup())
+        except (ValueError, AttributeError):
+            msg = bot.send_message(user.id, 'Неверный номер телефона. Для отмены регистрации нажмите /stop')
+            bot.register_next_step_handler(msg, get_phone)
+        except KeyError:
+            bot.send_message(user.id, 'Ошибка регистрации, начните заново /reg')
+    else:
+        bot.send_message(msg.chat.id, 'Регистрация прервана', reply_markup=get_markup())
 
 
 def getData(user, title):
@@ -115,6 +124,8 @@ def getData(user, title):
     })
 
 
+
+@run_async
 @bot.message_handler(commands=['info'])
 def info_message(msg):
     try:
@@ -125,6 +136,7 @@ def info_message(msg):
         bot.send_message(msg.chat.id, 'Вы еще не зарегистрированы, напишите /reg')
 
 
+@run_async
 @bot.message_handler(content_types=['text'])
 def get_text_messages(msg):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
